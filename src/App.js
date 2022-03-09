ea962a8c7b410d452cdd428730ef895f69a0c88e
cono07 <amy_oconnor07@hotmail.co.uk>
2022-03-09 12:46:25 +0000

    view single article

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import { ArticleList } from "./Components/ArticleList";
 import Heading from "./Components/Heading";
+import { SingleArticle } from "./Components/SingleArticle";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<ArticleList />} />
         <Route path="/articles" element={<ArticleList />} />
         <Route path="/articles/:topic_name" element={<ArticleList />} />
+        <Route path="/article/:article_id" element={<SingleArticle />} />
       </Routes>
     </>
   );
