99333b53f95c3e2c96010fa2723c2d6d94a57f47
cono07 <amy_oconnor07@hotmail.co.uk>
2022-04-18 17:27:06 +0100

    update article card with icons

--- src/Components/Articles/ArticleItem.jsx
+++ src/Components/Articles/ArticleItem.jsx
@@ -1,22 +1,27 @@
 import { Link } from "react-router-dom";
 import { formatDate } from "../../utils/formatDate";
+import commentImg from "../../images/comments-icon.png";
 
 export const ArticleItem = ({ article }) => {
   const date = article.created_at;
   const articleDate = formatDate(date);
+  const voteIcon = article.votes >= 0 ? "👍" : "👎";
 
   return (
     <article>
       <Link to={`/article/${article.article_id}`} className="ArticleCard">
         <h2 id="ArticleCard_article-title">{article.title}</h2>
         <dl>
-          <dt className="ArticleCard_author">Author: {article.author}</dt>
+          <dt className="ArticleCard_author">{article.author}</dt>
           <dt className="ArticleCard_date">{articleDate}</dt>
           <dt className="ArticleCard_detail">{article.topic}</dt>
-          <dt className="ArticleCard_detail">Votes: {article.votes}</dt>
           <dt className="ArticleCard_detail">
-            Comments: {article.comment_count}
+            {voteIcon} {article.votes}
           </dt>
+          <div className="ArticleCard_comments">
+            <img src={commentImg} className="comments-icon" alt="comments"></img>
+            <dt className="ArticleCard_detail">{article.comment_count}</dt>
+          </div>
         </dl>
       </Link>
     </article>
